1ed78e6db273f537b836883c28e1a249e7423b2e
AR2D <arturochi2@hotmail.com>
2016-11-13 00:36:51 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_login.xml
    
    ya cambia de splash al login

--- app/src/main/java/edu/alexandercd/hackatonapp/LoginActivity.java
+++ app/src/main/java/edu/alexandercd/hackatonapp/LoginActivity.java
@@ -1,13 +1,54 @@
 package edu.alexandercd.hackatonapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnFacebook, btnGoogle, btnRegistro, btnIniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnIniciar = (Button) findViewById(R.id.btn_iniciar_sesion);
+        btnFacebook = (Button) findViewById(R.id.btn_iniciar_sesion_facebook);
+        btnGoogle = (Button) findViewById(R.id.btn_iniciar_sesion_google);
+        btnRegistro = (Button) findViewById(R.id.btn_iniciar_sesion_registro);
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(LoginActivity.this, WelcomeActivity.class);
+                startActivity(next);
+                finish();
+            }
+        });
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(LoginActivity.this, WelcomeActivity.class);
+                startActivity(next);
+                finish();
+            }
+        });
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(LoginActivity.this, WelcomeActivity.class);
+                startActivity(next);
+                finish();
+            }
+        });
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(LoginActivity.this, WelcomeActivity.class);
+                startActivity(next);
+                finish();
+            }
+        });
     }
 }
