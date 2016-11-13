606ae772dd0e70529fc5a37bc6e251cfa7586064
AR2D <arturochi2@hotmail.com>
2016-11-12 20:35:45 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/edu/alexandercd/hackatonapp/LoginActivity.java
+++ app/src/main/java/edu/alexandercd/hackatonapp/LoginActivity.java
@@ -1,7 +1,7 @@
 package edu.alexandercd.hackatonapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
